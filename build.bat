676e0482d3751f11216212b3d0674c20a9715296
Fran Oviamionayi <fran.ovia@gmail.com>
2017-05-07 03:06:02 +0200

    Adding Ant and Travis

--- build.bat
+++ build.bat
@@ -1,13 +1,13 @@
-javac -Xlint -classpath "target" -d "target" src\main\java\jfnlite\Fn.java
-javac -Xlint:deprecation -classpath "target" -d "target" src\main\java\apktlog2pcap\ByteUtils.java
-javac -Xlint:deprecation -classpath "target" -d "target" src\main\java\apktlog2pcap\Pcap.java
-javac -Xlint:deprecation -classpath "target" -d "target" src\main\java\apktlog2pcap\LineIterator.java
-javac -Xlint:deprecation -classpath "target" -d "target" src\main\java\apktlog2pcap\LogProtoParser.java
-javac -Xlint:deprecation -classpath "target" -d "target" src\main\java\apktlog2pcap\ApktLog.java
-javac -Xlint:deprecation -classpath "target" -d "target" src\main\java\apktlog2pcap\AppCore.java
-javac -Xlint:deprecation -classpath "target" -d "target" src\main\java\apktlog2pcap\Cli.java
-javac -Xlint:deprecation -classpath "target" -d "target" src\main\java\apktlog2pcap\Gui.java
+javac -Xlint -classpath "bin" -d "bin" src\main\java\jfnlite\Fn.java
+javac -Xlint:deprecation -classpath "bin" -d "bin" src\main\java\apktlog2pcap\ByteUtils.java
+javac -Xlint:deprecation -classpath "bin" -d "bin" src\main\java\apktlog2pcap\Pcap.java
+javac -Xlint:deprecation -classpath "bin" -d "bin" src\main\java\apktlog2pcap\LineIterator.java
+javac -Xlint:deprecation -classpath "bin" -d "bin" src\main\java\apktlog2pcap\LogProtoParser.java
+javac -Xlint:deprecation -classpath "bin" -d "bin" src\main\java\apktlog2pcap\ApktLog.java
+javac -Xlint:deprecation -classpath "bin" -d "bin" src\main\java\apktlog2pcap\AppCore.java
+javac -Xlint:deprecation -classpath "bin" -d "bin" src\main\java\apktlog2pcap\Cli.java
+javac -Xlint:deprecation -classpath "bin" -d "bin" src\main\java\apktlog2pcap\Gui.java
 
-jar cvfm apktlog2pcap.jar src\main\resources\Manifest.txt -C "target" .
+jar cvfm apktlog2pcap.jar src\main\resources\Manifest.txt -C "bin" .
 
 pause
